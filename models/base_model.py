fc3179b899b852aaa8bf2a7a1f3fd8de4b8e2699
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2022-01-26 02:20:00 +0300

    working on base model and the init function

---
+++ models/base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the module that contains the base class which defines all common attributes/methods for other classes.
+"""
+
+import uuid
+
+Class BaseModel:
+	__id__=""
+	__created_at__=
+	__updated_at__=
+
+	def __init__(self, id):
+		id = uuid.uuid4()
+		created_at = 
+		updated_at=
