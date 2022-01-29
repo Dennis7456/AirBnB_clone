11b29ac99fc6694f558c9e64a88397da1760e9c1
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-01-30 02:59:08 +0300

    created the base model with instance attributes and methods

--- models/base_model.py
+++ models/base_model.py
@@ -3,14 +3,28 @@
 This is the module that contains the base class which defines all common attributes/methods for other classes.
 """
 
-import uuid
+import uuid, datetime
 
-Class BaseModel:
-	__id__=""
-	__created_at__=
-	__updated_at__=
+class BaseModel():
+	def __init__(self, id='', created_at='', updated_at=''):
+		self.id = str(uuid.uuid4())
+		self.created_at = str(datetime.datetime.utcnow().isoformat("T")) 
+		self.updated_at = str(datetime.datetime.utcnow().isoformat("T"))
 
-	def __init__(self, id):
-		id = uuid.uuid4()
-		created_at = 
-		updated_at=
+
+	def save(self):
+		self.updated_at = str(datetime.datetime.utcnow())
+
+	def to_dict(self):
+		basedict = self.__dict__
+		basedict['__class__'] = type(self).__name__
+		return basedict
+
+	def __str__(self):
+		class_name = type(self).__name__
+		id_string = str(self.id)
+		return "[" + class_name + "] (" + id_string + str(self.__dict__) + ")"
+
+myObject = BaseModel(1)
+print(myObject.__str__())
+#print(myObject.to_dict())
