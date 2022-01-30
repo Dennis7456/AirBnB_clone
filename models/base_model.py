103e9b7675c4a985c80b0cba18b5f58e8694ddd2
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-01-30 04:21:48 +0300

    Added args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,42 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+
 """
 This is the module that contains the base class
 which defines all common attributes/methods
 for other classes.
 """
 
-import uuid
-import datetime
-
 
 class BaseModel():
-    def __init__(self, id='', created_at='', updated_at=''):
-        self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.utcnow().isoformat("T"))
-        self.updated_at = str(datetime.datetime.utcnow().isoformat("T"))
+    """ Initi method """
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for key in kwargs:
+                if key == 'id':
+                    self.id = kwargs.get(key)
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'updated_at':
+                    self.updated_at = datetime.strptime(kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'my_number':
+                    self.my_number = kwargs.get(key)
+                if key == 'name':
+                    self.name = kwargs.get(key)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = str(datetime.datetime.utcnow())
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         basedict = self.__dict__
         basedict['__class__'] = type(self).__name__
+        basedict['updated_at'] = self.updated_at.isoformat("T")
+        basedict['created_at'] = self.created_at.isoformat("T")
         return basedict
 
     def __str__(self):
