cab633f589aacd21560e7d682e683728d1cb1311
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-01-31 03:37:48 +0300

    small changes

--- console.py
+++ console.py
@@ -2,14 +2,7 @@
 import cmd
 from shlex import shlex
 from models.base_model import BaseModel
-from models.user import User
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models import storage
-from models.engine.file_storage import FileStorage
+
 """This is the console module which contains the HBNB class
 which is the main entry point for the console"""
 
